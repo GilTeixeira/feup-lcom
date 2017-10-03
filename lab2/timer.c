fb732ab2d01b7ff6e3750602434a18f3080cde32
up201607926 <up201607926@fde639fb-d9cb-45d8-b5e7-4c576959c795>
2017-10-03 14:35:15 +0000

    timer_get_conf() completed
    git-svn-id: https://svn.fe.up.pt/repos/lcom1718-t6g08@4 fde639fb-d9cb-45d8-b5e7-4c576959c795

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,7 +6,7 @@ int timer_set_frequency(unsigned char timer, unsigned long freq) {
 	return 1;
 }
 
-int timer_subscribe_int(void) {
+int timer_subscribe_int(void ) {
 
 	return 1;
 }
@@ -21,41 +21,35 @@ void timer_int_handler() {
 }
 
 int timer_get_conf(unsigned char timer, unsigned char *st) {
+	unsigned long stout;
+	port_t timerport;
+	unsigned long timerselect;
 
-	unsigned long confRead;
-
-	 unsigned long timerStatus;
-
-	 /*
-	int timerToRead = atoi(timer);
-	int timerAddress;
-
-	switch (timerToRead) {
-	case 0:
-		timerAddress = TIMER_0;
+	switch (timer) {
+	case '0': //timer 0
+		timerport = TIMER_0;
+		timerselect = TIMER_SEL0;
 		break;
-	case 1:
-		timerAddress = TIMER_1;
+	case '1': //timer1
+		timerport = TIMER_1;
+		timerselect = TIMER_SEL0;
 		break;
-	case 2:
-		timerAddress = TIMER_2;
+	case '2': //timer2
+		timerport = TIMER_2;
+		timerselect = TIMER_SEL0;
 		break;
-
 	}
 
-	if (sys_inb(timerAddress, &confRead) == 0)
-		return 0;
-	else
-		return 1;
-		*/
+	stout = TIMER_RB_CMD | timerselect | TIMER_RB_STATUS_;
+	sys_outb(TIMER_CTRL, stout);
+	sys_inb(timerport, &st);
+	return 0;
 }
 
 int timer_display_conf(unsigned char conf) {
 
-	return 1;
-}
+	//develop
 
-int timer_test_time_base(unsigned long freq) {
 
 	return 1;
 }
@@ -66,6 +60,35 @@ int timer_test_int(unsigned long time) {
 }
 
 int timer_test_config(unsigned char timer) {
+	// testar as duas funções e avisar no caso de dar erro
+	if(timer_get_conf()!=0){
+	//se não der avisar o erro
+		return 1;
+	} else
+	{
+	//se der, testar o outro
+	if(timer_display_conf()!=0){
+	return 1;
+	}}
+
+
+	return 0;
+}
+
+int timer_test_time_base(unsigned long freq) {
+	unsigned long cenasoutbin,cenasoutbcd;
+cenasoutbin=cenasoutbin|BIT(2)|BIT(3)|BIT(6);
+
+timer_set_frequency();
+//cenasoutbin=00100110;
+    //cenasoutbcd=00100111;
+//USAR O timerfreq
+
+
+//enviar para 0x40 a informação com o timer que eu quero (timer 0, timer 1 , etc)
+
+//vector 0x08
+
 
 	return 1;
 }
