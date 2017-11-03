0927ffcf088944f0ca494d803ecb10ecdc97797d
Gil Teixeira <up201505735@fe.up.pt>
2017-11-03 17:00:37 +0000

    Lab4 Main concluded
    git-svn-id: https://svn.fe.up.pt/repos/lcom1718-t6g08@31 fde639fb-d9cb-45d8-b5e7-4c576959c795

---
+++ lab4/test4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int mouse_test_packet(unsigned short cnt){
+    /* To be completed ... */
+
+
+	return 0;
+}	
+
+int mouse_test_async(unsigned short idle_time){
+    /* To be completed ... */
+
+	return 0;
+}	
+
+int mouse_test_remote(unsigned long period, unsigned short cnt){
+    /* To be completed ... */
+
+	return 0;
+}	
+
+int mouse_test_gesture(short length){
+    /* To be completed ... */
+
+	return 0;
+}	
