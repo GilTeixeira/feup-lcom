04b6b484ce0fcb482f4e84cfd40a7950c0c24883
Gil Teixeira <up201505735@fe.up.pt>
2017-11-17 01:09:13 +0000

    Lab5 Main concluded
    git-svn-id: https://svn.fe.up.pt/repos/lcom1718-t6g08@53 fde639fb-d9cb-45d8-b5e7-4c576959c795

---
+++ lab5/test5.c
@@ -0,0 +1,38 @@
+void *video_test_init(unsigned short mode, unsigned short delay) {
+	
+	/* To be completed */
+	
+}
+
+
+int video_test_square(unsigned short x, unsigned short y, unsigned short size, unsigned long color) {
+	
+	/* To be completed */
+	
+}
+
+int video_test_line(unsigned short xi, unsigned short yi, 
+		           unsigned short xf, unsigned short yf, unsigned long color) {
+	
+	/* To be completed */
+	
+}
+	
+int test_xpm(char *xpm[], unsigned short xi, unsigned short yi) {
+	
+	/* To be completed */
+	
+}	
+
+int test_move(char *xpm[], unsigned short xi, unsigned short yi, unsigned short xf, unsigned short yf, short s, unsigned short f) {
+
+	/* To be completed */
+	
+}	
+
+int test_controller() {
+
+	/* To be completed */
+	
+}	
+	
